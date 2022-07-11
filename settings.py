08585c4f53d2606d5ad78e9182ab1213ffc77cfd
Ruslan Manoharan <57958158+Ru23-GitHub@users.noreply.github.com>
2022-07-11 12:35:08 -0700

    Set Up Level

---
+++ settings.py
@@ -0,0 +1,18 @@
+level_map = [
+'                                                ', #Row 0
+'                                                ', #Row 1
+'                                                ', #Row 2
+'                                                ', #Row 3
+'                                                ', #Row 4
+'                                                ', #Row 5
+'                                                ', #Row 6
+'                                                ', #Row 7
+'        X  XX                   X  XX           ', #Row 8
+'      XXX  XXXX               XXX  XXXX         ', #Row 9
+'XXXXXXXXX  XXXXXXXXXXXXXXXXXXXXXX  XXXXXXXXXXXXX'] #Row 10
+#012345678911111111112222
+#          01234567890123
+
+tile_size = 64
+screen_width = 1200
+screen_height = tile_size * len(level_map)
